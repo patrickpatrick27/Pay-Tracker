44432acfe5434b3e47d45c07eb04705506204d8e
Dave Patrick I. Bulaso <158995199+patrickpatrick27@users.noreply.github.com>
2026-01-31 09:53:06 +0800

    Updated settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -11,15 +11,15 @@ pluginManagement {
 
     repositories {
         google()
-        mavenCentral()
+        mavenCentral() // <--- FIX: Changed from maincentral()
         gradlePluginPortal()
     }
 }
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.2.1" apply false  // <--- UPDATED (Was 8.1.0)
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false // <--- UPDATED (Was 1.8.22)
+    id("com.android.application") version "8.6.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
 include(":app")
